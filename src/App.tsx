deaf8e942fa267cc3554c3919fde5630ebe57ea9
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-08-19 15:53:38 +0700

    basic position scale and button animation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { motion } from 'motion/react'
+
+function App() {
+
+  return (
+    <motion.div>
+      <h1>hello world</h1>
+    </motion.div>
+  )
+}
+
+export default App
